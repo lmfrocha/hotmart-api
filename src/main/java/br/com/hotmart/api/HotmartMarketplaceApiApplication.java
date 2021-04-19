6384de5c9a6946910efa83e792e8a8326d50523d
Rocha Lucas <l.rocha@reply.com>
2021-04-19 17:43:15 -0300

    First commit to repository

---
+++ src/main/java/br/com/hotmart/api/HotmartMarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotmart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartMarketplaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartMarketplaceApiApplication.class, args);
+	}
+
+}
